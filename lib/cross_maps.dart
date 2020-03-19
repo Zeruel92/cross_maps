847a0915151c489f8659002ebe2124ca673a113f
Gabriele Tramonte <gabry.t92@gmail.com>
2020-03-19 20:12:00 +0100

    title position

--- lib/cross_maps.dart
+++ lib/cross_maps.dart
@@ -7,7 +7,7 @@ import 'cross_maps_stub.dart'
     if (dart.library.html) 'web_maps.dart';
 
 abstract class CrossMaps {
-  Widget getMaps(double lat, double lng,String title);
+  Widget getMaps(double lat, double lng, String title);
   void setPolyline(List<double> lats, List<double> lngs);
   factory CrossMaps() => getCrossMaps();
 }
