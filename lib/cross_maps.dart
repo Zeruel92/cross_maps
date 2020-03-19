f4f4de5fcede95ef0563b0c1052e279a2d0ff049
Gabriele Tramonte <gabry.t92@gmail.com>
2020-03-19 19:34:20 +0100

    fix web import

--- lib/cross_maps.dart
+++ lib/cross_maps.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'cross_maps_stub.dart'
     if (dart.library.io) 'mobile_maps.dart'
-    if (dart.library.html) 'mobile_maps.dart';
+    if (dart.library.html) 'web_maps.dart';
 
 abstract class CrossMaps {
   Widget getMaps(double lat, double lng);
