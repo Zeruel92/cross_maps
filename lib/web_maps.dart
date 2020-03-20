34abdeb5a6dd88e32b6bfb779bdaf4caccdf7410
Gabriele Tramonte <gabry.t92@gmail.com>
2020-03-20 09:32:46 +0100

    trying rebinding map on add marker

--- lib/web_maps.dart
+++ lib/web_maps.dart
@@ -11,53 +11,65 @@ class WebMaps implements CrossMaps {
   static WebMaps get instance => _instance;
 
   GMap _gmap;
-  List<Marker> markers = <Marker>[];
+  List<Marker> markers;
 
-  WebMaps._WebMaps() {}
+  WebMaps._WebMaps() {
+    markers = <Marker>[];
+  }
 
   @override
   Widget getMaps(double lat, double lng, String title,
       {Function onTap, List<double> poly_lats, List<double> poly_lngs}) {
     String htmlId = "7";
-    if (_gmap != null) {
-      final mapOptions = MapOptions()
-        ..zoom = 8
-        ..center = LatLng(lat, lng)
-        ..mapTypeId = MapTypeId.TERRAIN
-        ..backgroundColor = '#ffffff';
+    final mapOptions = MapOptions()
+      ..zoom = 8
+      ..center = LatLng(lat, lng)
+      ..mapTypeId = MapTypeId.TERRAIN
+      ..backgroundColor = '#ffffff';
 
 // ignore: undefined_prefixed_name
-      ui.platformViewRegistry.registerViewFactory(htmlId, (int viewId) {
-        var elem = DivElement()
-          ..id = htmlId
-          ..style.width = "100%"
-          ..style.height = "100%"
-          ..style.border = 'none';
-        _gmap = GMap(elem, mapOptions);
-        _gmap.onClick.listen((event) {
-          onTap(event.latLng.lat, event.latLng.lng);
-        });
-        addMarker(lat, lng, title);
-        setPolyline(poly_lats, poly_lngs);
-        return elem;
+    ui.platformViewRegistry.registerViewFactory(htmlId, (int viewId) {
+      var elem = DivElement()
+        ..id = htmlId
+        ..style.width = "100%"
+        ..style.height = "100%"
+        ..style.border = 'none';
+      _gmap = GMap(elem, mapOptions);
+      _gmap.onClick.listen((event) {
+        onTap(event.latLng.lat, event.latLng.lng);
       });
-    } else {
-      _gmap.center = LatLng(lat, lng);
-      clearMarkers();
       addMarker(lat, lng, title);
-    }
+      setPolyline(poly_lats, poly_lngs);
+      return elem;
+    });
     return HtmlElementView(viewType: htmlId);
   }
 
   @override
   void addMarker(double lat, double lng, String title) {
-    Marker marker = Marker(MarkerOptions()
-      ..position = LatLng(lat, lng)
-      ..map = _gmap
-      ..title = "Posizione: $title"
-      ..clickable = true
-      ..draggable = true);
-    markers.add(marker);
+    String htmlId = "7";
+    final mapOptions = MapOptions()
+      ..zoom = 8
+      ..center = LatLng(lat, lng)
+      ..mapTypeId = MapTypeId.TERRAIN
+      ..backgroundColor = '#ffffff';
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(htmlId, (int viewId) {
+      var elem = DivElement()
+        ..id = htmlId
+        ..style.width = "100%"
+        ..style.height = "100%"
+        ..style.border = 'none';
+      _gmap = GMap(elem, mapOptions);
+      Marker marker = Marker(MarkerOptions()
+        ..position = LatLng(lat, lng)
+        ..map = _gmap
+        ..title = "Posizione: $title"
+        ..clickable = true
+        ..draggable = true);
+      markers.add(marker);
+      return elem;
+    });
   }
 
   void clearMarkers() {
